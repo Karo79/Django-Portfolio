a37386fcc7e2f9e44b86be1c6b47832327c43c65
Karo <karo.n1999@outlook.com>
2021-02-23 11:14:11 +0000

    added gitignore

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^yh_*j1t)*bn@^7x9*@4#%(%m%0tvc#ono5(l=-wd=8ckm^o_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karo79.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
